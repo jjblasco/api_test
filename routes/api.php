c61c07f205c82eb1f784ec4c00c55bf2d012ebd6
Jose Blasco <joseangelblasco.93@gmail.com>
2024-12-22 16:27:00 +0100

    add new route and test to check order text

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\ParentsesisController;
 use App\Http\Controllers\ShortUrlController;
 use App\Http\Middleware\ValidateBearerToken;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/v1/short-urls', [ShortUrlController::class, 'getJson'])
+        ->middleware([ValidateBearerToken::class]);
+
+Route::post('/check-order', [ParentsesisController::class, 'checkOrder'])
         ->middleware([ValidateBearerToken::class]);
