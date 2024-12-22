932f13369554d0336c05d89ecc50e4357a9fec55
Jose Blasco <joseangelblasco.93@gmail.com>
2024-12-22 14:08:10 +0100

    add barrertoken and test

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ShortUrlController;
+use App\Http\Middleware\ValidateBearerToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/v1/short-urls', [ShortUrlController::class, 'getJson']);
+Route::post('/v1/short-urls', [ShortUrlController::class, 'getJson'])
+        ->middleware([ValidateBearerToken::class]);
